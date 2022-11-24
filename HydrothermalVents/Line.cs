70bed8f541d1d90b39bc70d0470f3dcf1fac51fb
dsz <toxjr01@gmail.com>
2022-11-24 21:13:39 +0000

    Added Lines.

---
+++ HydrothermalVents/Line.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HydrothermalVents
+{
+    public class Line<T> where T : struct
+    {
+        private Vector<T> m_start;
+        private Vector<T> m_end;
+
+        public Line(Vector<T> start, Vector<T> end)
+        {
+            m_start = start;
+            m_end = end;
+        }
+    }
+}
---
+++ HydrothermalVentsTests/LineTest.cs
@@ -0,0 +1,18 @@
+using System.Numerics;
+using HydrothermalVents;
+
+namespace BusinessLogicTests
+{
+    [TestClass]
+    public class LineTest
+    {
+        [TestMethod]
+        public void TestCTOR()
+        {
+            Vector<int> start = new Vector<int>();
+            Vector<int> end = new Vector<int>();
+
+            Line<int> line;
+        }
+    }
+}
