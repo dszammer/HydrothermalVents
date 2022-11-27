1e23f220627bec4d119e91735f57fdf4c71aeefe
dsz <toxjr01@gmail.com>
2022-11-27 21:36:16 +0000

    Added Crossing class.

---
+++ HydrothermalVents/Crossing.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace HydrothermalVents
+{
+    public class Crossing<T> where T : struct
+    {
+        public Crossing()
+        {
+            m_position = new T[] { };
+            m_lines = new List<Linesegment<T>>();
+        }
+        public Crossing(T[] position, List<Linesegment<T>> lines)
+        {
+            m_position = position;
+            m_lines = lines;
+        }
+
+        public T[] Position { get => m_position; set => m_position = value; }
+        public List<Linesegment<T>> Lines { get => m_lines; set => m_lines = value; }
+
+
+        public void AddLinesegement(ref Linesegment<T> line)
+        {
+            m_lines.Add(line);
+        }
+
+        private T[] m_position;
+        private List<Linesegment<T>> m_lines;
+    }
+}
---
+++ HydrothermalVentsTests/CrossingTest.cs
@@ -0,0 +1,67 @@
+﻿using HydrothermalVents;
+
+namespace HydrothermalVentsTests
+{
+    [TestClass]
+    public class CrossingTest
+    {
+        [TestMethod]
+        public void TestCTOR()
+        {
+            Crossing<int> crossdef = new Crossing<int>();
+
+            int[] pos = new int[] {};
+            List<Linesegment<int>> listLines = new List<Linesegment<int>>();
+            
+            Crossing<int> cross = new Crossing<int>(pos,listLines);
+        }
+        [TestMethod]
+        public void TestPositionGetSet()
+        {
+            Crossing<int> cross = new Crossing<int>();
+            cross.Position = new int[] { 10, 20 };
+
+            Assert.IsTrue(cross.Position[0] == 10, "start should be the new vector.");
+            Assert.IsTrue(cross.Position[0] == 10, "start should be the new vector.");
+
+            List<Linesegment<int>> listLines = new List<Linesegment<int>>();
+            Linesegment<int> line = new Linesegment<int>(new int[] { 1, 2 }, new int[] { 3, 4 });
+            listLines.Add(line);
+
+            cross.Lines = listLines;
+
+            Assert.IsTrue(cross.Position[0] == 10, "start should be the new vector.");
+            Assert.IsTrue(cross.Position[0] == 10, "start should be the new vector.");
+
+        }
+
+        [TestMethod]
+        public void TestLinesGetSet()
+        {
+            Crossing<int> cross = new Crossing<int>();
+
+            List<Linesegment<int>> listLines = new List<Linesegment<int>>();
+            Linesegment<int> line = new Linesegment<int>(new int[] { 1, 2 }, new int[] { 3, 4 });
+            listLines.Add(line);
+
+            cross.Lines = listLines;
+
+            Assert.IsTrue(cross.Lines[0].Start[0] == 1, "start should be the new vector.");
+            Assert.IsTrue(cross.Lines[0].End[1] == 4, "start should be the new vector.");
+
+        }
+
+        [TestMethod]
+        public void TestAddLine()
+        {
+            Crossing<int> cross = new Crossing<int>();
+            Linesegment<int> line = new Linesegment<int>(new int[] { 1, 2 }, new int[] { 3, 4 });
+            cross.AddLinesegement(ref line);
+
+            Assert.IsTrue(cross.Lines[0].Start[0] == 1, "start should be the new vector.");
+            Assert.IsTrue(cross.Lines[0].End[1] == 4, "start should be the new vector.");
+        }
+
+        
+    }
+}
