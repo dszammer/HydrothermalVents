85bdcbe196102c2bd3475772ddf0a6e75e5485b5
David Szammer <d.szammer@gmail.com>
2025-02-27 15:32:17 +0100

    fixed crossings doky

--- HydrothermalVents/BusinessLogic/Crossing.cs
+++ HydrothermalVents/BusinessLogic/Crossing.cs
@@ -15,8 +15,11 @@ namespace HydrothermalVents.BusinessLogic
                                 where T : class
     {
         /// <summary>
-        /// Default CTOR
+        /// Initializes a new instance of the <see cref="Crossing{U, T}"/> class with default values.
         /// </summary>
+        /// <remarks>
+        /// This constructor initializes the position array to an empty array and the elements list to an empty list.
+        /// </remarks>
         public Crossing()
         {
             m_position = new U[] { };
@@ -24,27 +27,46 @@ namespace HydrothermalVents.BusinessLogic
         }
 
         /// <summary>
-        /// CTOR
-        /// param position: Position of the crossing of the elements.
-        /// param elements: Points that cross in position.
+        /// Initializes a new instance of the <see cref="Crossing{U, T}"/> class with specified position and elements.
         /// </summary>
+        /// <param name="position">The position of the crossing of the elements.</param>
+        /// <param name="elements">The elements that cross at the specified position.</param>
+        /// <remarks>
+        /// This constructor sets the position and elements of the crossing point.
+        /// </remarks>
         public Crossing(U[] position, List<T> elements)
         {
             m_position = position;
             m_elements = elements;
         }
 
-        public U[] Position { get => m_position; set => m_position = value; }
-        public List<T> Elements { get => m_elements; set => m_elements = value; }
-
         /// <summary>
-        /// In case an additional element is found that also crosses the position it can be added here.
-        /// param element: element to add.
+        /// Adds an additional element to the list of crossing elements.
         /// </summary>
+        /// <param name="element">The element to add to the crossing elements list.</param>
+        /// <remarks>
+        /// This method is used to add an element that also crosses the current position.
+        /// </remarks>
         public void AddElement(T element)
         {
             m_elements.Add(element);
         }
+
+        /// <summary>
+        /// Gets or sets the position of the crossing elements.
+        /// </summary>
+        /// <value>
+        /// An array of type <see cref="U"/> representing the position of the crossing elements.
+        /// </value>
+        public U[] Position { get => m_position; set => m_position = value; }
+
+        /// <summary>
+        /// Gets or sets the list of elements that cross at the specified position.
+        /// </summary>
+        /// <value>
+        /// A list of type <see cref="T"/> representing the elements that cross at the specified position.
+        /// </value>
+        public List<T> Elements { get => m_elements; set => m_elements = value; }
 
         private U[] m_position;
         private List<T> m_elements;
